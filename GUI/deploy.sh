e444cbec7db62df5411fcf09c292174ae1b28986
SamuWrob <samuel.wrob@gmail.com>
2021-01-20 18:13:04 +0100

    modify deploy.sh

--- GUI/deploy.sh
+++ GUI/deploy.sh
@@ -2,10 +2,10 @@
 # A simple django project deployment script for jenkins
 
 BUILD_ROOT="/var/lib/jenkins/workspace/test/"
-MNGR=${BUILD_ROOT}"manage.py"
+MNGR="/var/lib/jenkins/worskspace/test/manage.py"
 
 echo "Sourcing python env..."
-source ${BUILD_ROOT}"BAMLenv/bin/activate"
+source "var/lib/jenkins/workspace/test/BAMLenv/bin/activate"
 
 pip install -r requirements.txt
 
